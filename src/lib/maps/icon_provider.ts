de6186a3270b372b606144030d53ee9631429752
Kate Hobbs <kate.hobbs4@gmail.com>
2025-05-03 14:51:34 +1000

    updated trail icons & layout

--- src/lib/maps/icon_provider.ts
+++ src/lib/maps/icon_provider.ts
@@ -1,17 +1,13 @@
 import L from 'leaflet'
 
-import markerIcon from './assets/marker-icon.png?url'
-import markerIcon2x from './assets/marker-icon-2x.png?url'
-import markerShadow from './assets/marker-shadow.png?url'
+import locationPin from '../../assets/icons/location-pin.png?url'
 
 export class IconProvider {
   createPinIcon() {
     return L.icon({
-      iconUrl: markerIcon,
-      iconRetinaUrl: markerIcon2x,
-      iconSize: [25, 41],
-      iconAnchor: [12.5, 41],
-      shadowUrl: markerShadow,
+      iconUrl: locationPin,
+      iconSize: [40, 48],
+      iconAnchor: [15, 30],
     })
   }
 }
